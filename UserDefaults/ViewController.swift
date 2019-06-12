a5ec445a1b3bc45112e10fd94173ebe2bb29185c
minutoni <minutoni105@gmail.com>
2019-06-12 14:32:21 +0900

    多分できた〜

--- UserDefaults/ViewController.swift
+++ UserDefaults/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var text1:UITextField!
+    @IBOutlet var text2:UITextField!
+    
+    var testTextArray: Dictionary<String,String> = [:]
+    
+    var save : Dictionary<String,String> = [:]
+    //UserDefaultsのインスタンス
+    let userDefaults = UserDefaults.standard
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if userDefaults.dictionary(forKey: "Data") != nil {
+            testTextArray = userDefaults.object(forKey: "Data") as! Dictionary<String, String>
+            text1.text = testTextArray["kudamono"]
+            text2.text = testTextArray["yasai"]
+        }
+    }
+    
+    @IBAction func buttonTapped(_ sender:Any){
+        save = ["kudamono": text1.text!,"yasai":text2.text!]
+        userDefaults.set(save, forKey: "Data")
+        userDefaults.set(text1.text, forKey: "kudamono")
+        userDefaults.set(text2.text, forKey: "yasai")
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //outputText.text = inputText.text
+        self.view.endEditing(true)
+    }
 
 }
 
